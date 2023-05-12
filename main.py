3de62a94b3bb2c12f9cbcfc76c12c968ff638dbb
M3lonpanda <justin.m.li@stonybrook.edu>
2023-05-12 21:29:38 +0000

    revised main.py and requirements to make program work

--- main.py
+++ main.py
@@ -2,35 +2,33 @@ from fastapi import FastAPI
 import pandas as pd 
 
 df = pd.read_csv('./data/diagnoses2018.csv')
-df.columns
+app = FastAPI() 
 
-app = FastAPI() # This is what will be refrenced in config
+@app.get('/')
+async def root():
+    return {'this is a API service for MN ICD code details'}
 
-@app.route('/', methods=["GET"])
-def home():
-    return 'this is a API service for MN ICD code details'
-
-@app.route('/preview', methods=["GET"])
-def preview():
+@app.get('/preview')
+async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
-    return result
+    return {result}
 
-@app.route('/serviceyear/<value>', methods=['GET'])
-def serviceyear(value):
+@app.get('/serviceyear/{value}')
+async def serviceyear(value):
     print('value: ', value)
     filtered = df[df['service_year'] == value]
     if len(filtered) <= 0:
-        return 'There is nothing here'
+        return {'There is nothing here'}
     else: 
-        return filtered.to_json(orient="records")
+        return {filtered.to_json(orient="records")}
 
-@app.route('/serviceyear/<value>/sex/<value2>')
-def serviceyear2(value, value2):
+@app.get('/serviceyear/{value}/sex/{value2}')
+async def serviceyear2(value, value2):
     filtered = df[df['service_year'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
-        return 'There is nothing here'
+        return {'There is nothing here'}
     else: 
-        return filtered2.to_json(orient="records")    
+        return {filtered2.to_json(orient="records")}    
     

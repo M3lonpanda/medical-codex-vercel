6a800e204d38c342b37e82226fc0b6d1cbe28142
M3lonpanda <justin.m.li@stonybrook.edu>
2023-05-12 20:29:38 +0000

    made modifications to the main.py file

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from time import time
-from fastapi import FastAPI, __version__
+from fastapi import FastAPI, request
+import pandas as pd 
+
+df = pd.read_csv('./data/diagnoses2018.csv')
+df.columns
 
 app = FastAPI() # This is what will be refrenced in config
 
-@app.get('/')
-async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is a API service for MN ICD code details'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/serviceyear/<value>', methods=['GET'])
+def serviceyear(value):
+    print('value: ', value)
+    filtered = df[df['service_year'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.route('/serviceyear/<value>/sex/<value2>')
+def serviceyear2(value, value2):
+    filtered = df[df['service_year'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")    
+    
